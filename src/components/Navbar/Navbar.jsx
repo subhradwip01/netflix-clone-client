7e5ac596bc37281c07dfc4d56600303965df2479
subhradwip01 <subhradwip01@gmail.com>
2022-05-06 23:57:23 +0530

    Asstets added and navbar structure ccreated
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,59 @@
+import React,{useState} from 'react'
+import logo from "../../assets/netflix.png"
+import {NavLink} from "react-router-dom"
+import profile from "../../assets/profile.png"
+import classes from "./navbar.module.css"
+import {FaAngleDown} from "react-icons/fa"
+const Navbar = () => {
+const [showProfileDetails,setShowProfileDetails]=useState(false)
+  const navMenu=[{
+      name:"Homepage",
+      path:"/"
+  },
+{
+    name:"Series",
+    path:"/series"
+},
+{
+    name:"Movie",
+    path:"/movies"
+},
+{
+    name:"My List",
+    path:"/mylist"
+} ]
+
+const showDetailsMenu=()=>{
+  setShowProfileDetails(p=>!p)
+}
+  return (
+    <nav className={classes.navbar}>
+        <div className={classes.leftnav}>
+          <img className={classes.logo} src={logo} alt=""/>
+          <ul className={classes.navlist}>
+            {navMenu.map(item=>(
+                <li>
+                    <NavLink to={item.path}>{item.name}</NavLink>
+                
+                </li>
+            ))}
+          </ul>
+        </div>
+        <div className={classes.navright}>
+            <img className={classes.profile} src={profile} alt="profile pic"/>
+            <div className={classes.profile_details}>
+            <FaAngleDown className={`${classes.arrow} ${showProfileDetails ? classes.arrowUp : ""}` } size={20} onClick={showDetailsMenu}/>
+            {/* <div className={classes.profile_details_menu}>
+               <NavLink to="/profile">Profile</NavLink>
+               <span>Logout</span>
+            </div> */}
+            </div>
+        </div>
+        <div>
+
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
