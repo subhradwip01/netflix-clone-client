c74333a302eb347453355ee12cadd4efcd7f8737
subhradwip01 <subhradwip01@gmail.com>
2022-05-10 00:48:09 +0530

    Fetured complete
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import imgTitle from "../../assets/mh.png"
 import classes from "./Featured.module.css"
-const bgMovie = "../../assets/bg3.jpg"
+import { FaPlay, FaInfoCircle } from "react-icons/fa"
+import bgMovie from "../../assets/bg3.jpg"
 
 const Featured = () => {
   return (
-    <div className={classes.featured} >
+    <div className={classes.featured} style={{"backgroundImage":`url(${bgMovie})`}} >
       <div className={classes.movie_details}>
         <img className={classes.imgTitle} src={imgTitle} />
         <p className={classes.desc}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas adipisci repudiandae at inventore amet magnam odit consequuntur soluta. Nulla est nobis ut sint velit quas ratione dolore harum? Vitae, pariatur.</p>
         <div className={classes.actions}>
-          <button className={classes.play}>Play</button>
-          <button className={classes.info}>Info</button>
+          <button className={classes.play}><FaPlay size={16} color="white" />Play</button>
+          <button className={classes.info}><FaInfoCircle size={17} color="white" />Info</button>
         </div>
       </div>
 
