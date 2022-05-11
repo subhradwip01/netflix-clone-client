36247799e6db92bd454868a1fa1a9c492e4c04f9
subhradwip01 <subhradwip01@gmail.com>
2022-05-11 23:30:46 +0530

    List component done
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -34,22 +34,22 @@ const List = () => {
           style={{ display: !isMovied ? "none" : "block" }}
         />
         <div ref={listRef} className={classes.container}>
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
+          <ListItem index={0}/>
+          <ListItem index={1}/>
+          <ListItem index={2}/>
+          <ListItem index={3}/>
+          <ListItem index={4}/>
+          <ListItem index={5}/>
+          <ListItem index={6}/>
+          <ListItem index={7}/>
+          <ListItem index={8}/>
+          <ListItem index={9}/>
+          <ListItem index={10}/>
+          <ListItem index={11}/>
+          <ListItem index={12}/>
+          <ListItem index={13}/>
+          <ListItem index={14}/>
+          <ListItem index={15}/>
         </div>
         <FaAngleRight
           className={`${classes.sliderArrow} ${classes.right}`}
--- src/components/ListItem/ListItem.jsx
+++ src/components/ListItem/ListItem.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React,{useState} from 'react'
 import classes from "./ListItem.module.css"
+import {IoPlaySharp,IoAdd} from "react-icons/io5"
 
-const ListItem = () => {
+
+const ListItem = ({index}) => {
+  const [isHover,setIsHovered]=useState(false)
+  const trailer =
+    "https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761";
   return (
-    <div className={classes.listItem}>
-        item
-    </div>
+    <div className={classes.listItem} style={{"left":`${isHover && 230*index}px`}} onMouseEnter={()=>setIsHovered(true)} onMouseLeave={()=>setIsHovered(false)}>
+        <img src="https://w0.peakpx.com/wallpaper/160/389/HD-wallpaper-spider-man-spider-man-2-doctor-octopus.jpg" alt="" srcset="" />
+        {isHover && 
+        <>
+        <video src={trailer} autoPlay={true} loop></video>
+        <div className={classes.itemInfo}>
+          <div className={classes.icons}>
+            <IoPlaySharp color='white' size={30} className={classes.icon}/>
+            <IoAdd color='white' size={30} className={classes.icon}/>
+          </div>
+          <div className={classes.itemInfoTop}>
+              <spna>1 hour 14 mins</spna>
+              <span className={classes.limit}>+16</span>
+              <spna>1999</spna>
+          </div>
+          <div className={classes.desc}>
+            Lorem, ipsum dolor sit amet consectetur adipisicing
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia iure
+          </div>
+          <div className={classes.genre}>Action</div>
+          
+        </div>
+        </>}
+    </div >
   )
 }
 
