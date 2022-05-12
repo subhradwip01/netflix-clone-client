928c0a586caa28bd03fcd653be4b6879389d4425
subhradwip01 <subhradwip01@gmail.com>
2022-05-12 23:59:59 +0530

    Watch component structure created
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

--- src/pages/Watch.js
+++ src/pages/Watch.js
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import classes from "./Watch.module.css"
+import {IoArrowBack} from "react-icons/io5"
 const Watch = () => {
+  const trailer =
+    "https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761";
   return (
-    <div>Watch</div>
+    <div className={classes.watch}>
+      <div className={classes.back}>
+        <IoArrowBack size={20} color="white"/>
+      </div>
+      <video className={classes.video} autoPlay progress controls src={trailer}/>
+    </div>
   )
 }
 
-export default Watch
+export default Watch 
