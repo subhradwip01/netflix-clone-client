7680643c4bf8c89c45290c118da3fe9e1b25fb09
subhradwip01 <subhradwip01@gmail.com>
2022-05-15 21:48:06 +0530

    Navbar for login page
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,73 +1,77 @@
-import React,{useState,useRef} from 'react'
+import React, { useState, useRef } from 'react'
 import logo from "../../assets/netflix.png"
-import {NavLink} from "react-router-dom"
+import { NavLink } from "react-router-dom"
 import profile from "../../assets/profile.png"
 import classes from "./navbar.module.css"
-import {FaAngleDown} from "react-icons/fa"
+import { FaAngleDown } from "react-icons/fa"
 const Navbar = () => {
-const [showProfileDetails,setShowProfileDetails]=useState(false)
- const navRef=useRef()
-  const navMenu=[{
-      name:"Homepage",
-      path:"/"
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [showProfileDetails, setShowProfileDetails] = useState(false)
+  const navRef = useRef()
+  const navMenu = [{
+    name: "Homepage",
+    path: "/"
   },
-{
-    name:"Series",
-    path:"/series"
-},
-{
-    name:"Movie",
-    path:"/movies"
-},
-{
-    name:"My List",
-    path:"/mylist"
-} ]
+  {
+    name: "Series",
+    path: "/series"
+  },
+  {
+    name: "Movie",
+    path: "/movies"
+  },
+  {
+    name: "My List",
+    path: "/mylist"
+  }]
 
-const navColorHandler=()=>{
-  console.log(window.screenY)
-  if(window.scrollY>=40){
-    navRef.current.style.background="black"
-  }else{
-    navRef.current.style.background="linear-gradient(to top, transparent 0%, rgb(0, 0, 0, 0.3) 50%)"
+  const navColorHandler = () => {
+    console.log(window.screenY)
+    if (window.scrollY >= 40) {
+      navRef.current.style.background = "black"
+    } else {
+      navRef.current.style.background = "linear-gradient(to top, transparent 0%, rgb(0, 0, 0, 0.3) 50%)"
+    }
   }
-}
 
-window.addEventListener("scroll",navColorHandler)
+  window.addEventListener("scroll", navColorHandler)
 
-const showDetailsMenu=()=>{
-  setShowProfileDetails(p=>!p)
-}
+  const showDetailsMenu = () => {
+    setShowProfileDetails(p => !p)
+  }
   return (
     <nav className={classes.navbar} ref={navRef}>
-        <div className={classes.container}>
+      <div className={classes.container}>
         <div className={classes.leftnav}>
-          <img className={classes.logo} src={logo} alt=""/>
-          <ul className={classes.navlist}>
-            {navMenu.map(item=>(
-                <li>
-                    <NavLink to={item.path}>{item.name}</NavLink>
-                
-                </li>
+          <img className={classes.logo} src={logo} alt="" />
+          {isLoggedIn && <ul className={classes.navlist}>
+            {navMenu.map(item => (
+              <li>
+                <NavLink to={item.path}>{item.name}</NavLink>
+
+              </li>
             ))}
-          </ul>
+          </ul>}
         </div>
         <div className={classes.navright}>
-            <img className={classes.profile} src={profile} alt="profile pic"/>
-            <div className={classes.profile_details}>
-            <FaAngleDown className={`${classes.arrow} ${showProfileDetails ? classes.arrowUp : ""}` } size={20} onClick={showDetailsMenu} color="white"/>
+          {isLoggedIn && <><img className={classes.profile} src={profile} alt="profile pic" />
+          <div className={classes.profile_details}>
+            <FaAngleDown className={`${classes.arrow} ${showProfileDetails ? classes.arrowUp : ""}`} size={20} onClick={showDetailsMenu} color="white" />
 
             {showProfileDetails && <div className={classes.profile_details_menu}>
-               <li><NavLink to="/profile">Profile</NavLink></li>
-               <li>Logout</li>
+              <li><NavLink to="/profile">Profile</NavLink></li>
+              <li>Logout</li>
             </div>}
 
-            </div>
+          </div></>}
+          {!isLoggedIn && <>
+           <button className={classes.signIn}>Sign In</button>
+          </>}
         </div>
         <div>
 
         </div>
-        </div>
+      </div>
     </nav>
   )
 }
