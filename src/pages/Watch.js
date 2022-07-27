f25523e99c83b7977713ab6e996302af1783801f
subhradwip01 <subhradwip01@gmail.com>
2022-07-27 22:30:57 +0530

    Home btton link added
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

--- src/pages/Watch.js
+++ src/pages/Watch.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import classes from "./Watch.module.css"
 import {IoArrowBack} from "react-icons/io5"
-import { useLocation } from 'react-router-dom'
+import { useLocation,Link } from 'react-router-dom'
 const Watch = () => {
   const location=useLocation();
   const movie=location.state.movie;
   return (
     <div className={classes.watch}>
       <div className={classes.back}>
-        <IoArrowBack size={20} color="white"/> Home
+        <Link to="/"><IoArrowBack size={20} color="white"/> Home</Link>
       </div>
       <video className={classes.video} autoPlay progress controls src={movie.video}/>
     </div>
