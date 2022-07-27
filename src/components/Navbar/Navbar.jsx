426c781174ef0fedb98977f4927c57f0cee9092b
subhradwip01 <subhradwip01@gmail.com>
2022-07-27 21:56:01 +0530

    Refactoring navbar
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,78 +1,101 @@
-import React, { useState, useRef } from 'react'
-import logo from "../../assets/netflix.png"
-import { NavLink ,Link} from "react-router-dom"
-import profile from "../../assets/profile.png"
-import classes from "./navbar.module.css"
-import { FaAngleDown } from "react-icons/fa"
-const Navbar = ({user}) => {
-  const [showProfileDetails, setShowProfileDetails] = useState(false)
-  const navRef = useRef()
-  const navMenu = [{
-    name: "Homepage",
-    path: "/"
-  },
-  {
-    name: "Series",
-    path: "/series"
-  },
-  {
-    name: "Movie",
-    path: "/movies"
-  },
-  {
-    name: "My List",
-    path: "/mylist"
-  }]
+import React, { useState, useRef } from "react";
+import logo from "../../assets/netflix.png";
+import { NavLink, Link } from "react-router-dom";
+import profile from "../../assets/profile.png";
+import classes from "./navbar.module.css";
+import { FaAngleDown } from "react-icons/fa";
+const Navbar = ({ user }) => {
+  const [showProfileDetails, setShowProfileDetails] = useState(false);
+  const navRef = useRef();
+  const navMenu = [
+    {
+      name: "Homepage",
+      path: "/",
+    },
+    {
+      name: "Series",
+      path: "/series",
+    },
+    {
+      name: "Movie",
+      path: "/movies",
+    },
+    {
+      name: "My List",
+      path: "/mylist",
+    },
+  ];
+
+  console.log(user)
 
   const navColorHandler = () => {
-    console.log(window.screenY)
+    console.log(window.screenY);
     if (window.scrollY >= 40) {
-      navRef.current.style.background = "black"
+      navRef.current.style.background = "black";
     } else {
-      navRef.current.style.background = "linear-gradient(to top, transparent 0%, rgb(0, 0, 0, 0.3) 50%)"
+      navRef.current.style.background =
+        "linear-gradient(to top, transparent 0%, rgb(0, 0, 0, 0.3) 50%)";
     }
-  }
+  };
 
-  window.addEventListener("scroll", navColorHandler)
+  window.addEventListener("scroll", navColorHandler);
 
   const showDetailsMenu = () => {
-    setShowProfileDetails(p => !p)
-  }
+    setShowProfileDetails((p) => !p);
+  };
   return (
     <nav className={classes.navbar} ref={navRef}>
       <div className={classes.container}>
         <div className={classes.leftnav}>
           <img className={classes.logo} src={logo} alt="" />
-          {user && <ul className={classes.navlist}>
-            {navMenu.map(item => (
-              <li>
-                <NavLink to={item.path}>{item.name}</NavLink>
-
-              </li>
-            ))}
-          </ul>}
+          {user && (
+            <ul className={classes.navlist}>
+              {navMenu.map((item) => (
+                <li>
+                  <NavLink to={item.path}>{item.name}</NavLink>
+                </li>
+              ))}
+            </ul>
+          )}
         </div>
         <div className={classes.navright}>
-          {user && <><img className={classes.profile} src={user.profilePic || profile} alt="profile pic" />
-          <div className={classes.profile_details}>
-            <FaAngleDown className={`${classes.arrow} ${showProfileDetails ? classes.arrowUp : ""}`} size={20} onClick={showDetailsMenu} color="white" />
+          { user ? (
+            <>
+              <img
+                className={classes.profile}
+                src={user.profilePic || profile}
+                alt="profile pic"
+              />
+              <div className={classes.profile_details}>
+                <FaAngleDown
+                  className={`${classes.arrow} ${
+                    showProfileDetails ? classes.arrowUp : ""
+                  }`}
+                  size={20}
+                  onClick={showDetailsMenu}
+                  color="white"
+                />
 
-            {showProfileDetails && <div className={classes.profile_details_menu}>
-              <li><NavLink to="/profile">Profile</NavLink></li>
-              <li>Logout</li>
-            </div>}
-
-          </div></>}
-          {!user && <>
-           <Link className={classes.signIn} to="/login">Sign In</Link>
-          </>}
+                {showProfileDetails && (
+                  <div className={classes.profile_details_menu}>
+                    <li>
+                      <NavLink to="/profile">Profile</NavLink>
+                    </li>
+                    <li>Logout</li>
+                  </div>
+                )}
+              </div>
+            </>
+          ) : (
+            <button className={classes.signIn} to="/login">
+              Sign In
+            </button>
+          )}
         </div>
-        <div>
-
-        </div>
+        <div></div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
