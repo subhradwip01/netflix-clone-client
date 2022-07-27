c0cea4869e30a705926efd567a284f40809702b7
subhradwip01 <subhradwip01@gmail.com>
2022-07-27 21:56:50 +0530

    Watch component done
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

--- src/pages/Watch.js
+++ src/pages/Watch.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import classes from "./Watch.module.css"
 import {IoArrowBack} from "react-icons/io5"
+import { useLocation } from 'react-router-dom'
 const Watch = () => {
-  const trailer =
-    "https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761";
+  const location=useLocation();
+  const movie=location.state.movie;
   return (
     <div className={classes.watch}>
       <div className={classes.back}>
         <IoArrowBack size={20} color="white"/> Home
       </div>
-      <video className={classes.video} autoPlay progress controls src={trailer}/>
+      <video className={classes.video} autoPlay progress controls src={movie.video}/>
     </div>
   )
 }
