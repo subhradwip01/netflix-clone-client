0a8d1e244de21eebd932d198e0a855357b6d7e73
subhradwip01 <subhradwip01@gmail.com>
2022-08-07 10:31:24 +0530

    footer Done
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,38 +1,43 @@
 import React from 'react'
 import classes from "./Footer.module.css"
+import {FaInstagram,FaTwitter,FaFacebookF,FaYoutube} from "react-icons/fa"
 const Footer = () => {
   return (
     <div className={classes.footer}>
+        <div className={classes.socialMenu}>
+            <span className={classes.socialIcon}><FaFacebookF/></span>
+            <span className={classes.socialIcon}><FaInstagram/></span>
+            <span className={classes.socialIcon}><FaTwitter/></span>
+            <span className={classes.socialIcon}><FaYoutube/></span>
+        </div>
         <div className={classes.footerMenu}>
             <div className={classes.footerSubmenu}>
                 <ul className={classes.menuList}>
+                    <li className={classes.menuListItem}>Audio and Subtitles</li>
                     <li className={classes.menuListItem}>Media Center</li>
-                    <li className={classes.menuListItem}>Media Center</li>
-                    <li className={classes.menuListItem}>Media Center</li>
-                    <li className={classes.menuListItem}>Media Center</li>
+                    <li className={classes.menuListItem}>Privacy</li>
+                    <li className={classes.menuListItem}>Contact Us</li>
                 </ul>
             </div>
             <div className={classes.footerSubmenu}>
                 <ul className={classes.menuList}>
-                    <li className={classes.menuListItem}>Media Center</li>
-                    <li className={classes.menuListItem}>Media Center</li>
-                    <li className={classes.menuListItem}>Media Center</li>
-                    <li className={classes.menuListItem}>Media Center</li>
+                    <li className={classes.menuListItem}>Audio Description</li>
+                    <li className={classes.menuListItem}>Investor Relations</li>
+                    <li className={classes.menuListItem}>Legal Notice</li>
                 </ul>
             </div>
             <div className={classes.footerSubmenu}>
                 <ul className={classes.menuList}>
-                    <li className={classes.menuListItem}>Media Center</li>
-                    <li className={classes.menuListItem}>Media Center</li>
-                    <li className={classes.menuListItem}>Media Center</li>
-                    <li className={classes.menuListItem}>Media Center</li>
+                    <li className={classes.menuListItem}>Help Centre</li>
+                    <li className={classes.menuListItem}>Jobs</li>
+                    <li className={classes.menuListItem}>Cookie Preferences</li>
                 </ul>
             </div>
             <div className={classes.footerSubmenu}>
                 <ul className={classes.menuList}>
-                    <li className={classes.menuListItem}>Media Center</li>
-                    <li className={classes.menuListItem}>Media Center</li>
-                    <li className={classes.menuListItem}>Media Center</li>
+                    <li className={classes.menuListItem}>Gift Card</li>
+                    <li className={classes.menuListItem}>Terms of Use</li>
+                    <li className={classes.menuListItem}>Corporate Information</li>
                     <li className={classes.menuListItem}>Media Center</li>
                 </ul>
             </div>
